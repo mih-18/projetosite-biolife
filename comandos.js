37b908a49a7a3d34e204c09d7120f1faf550f545
mih-18 <132575067+mih-18@users.noreply.github.com>
2023-11-10 12:25:56 -0300

    Add files via upload

--- comandos.js
+++ comandos.js
@@ -1,2 +1,29 @@
-console
-let
+let botao = document.querySelector(".btn");
+
+botao.addEventListener("click", function(event){
+    event.preventDefault();
+
+    let nome= document.getElementById("nome").value;
+    let email= document.getElementById("e-mail").value;
+    let telefone= document.getElementById("telefone").value;
+    let mensagem= document.getElementById("mensagem").value;
+
+    console.log(nome);
+    console.log(email);
+    console.log(telefone);
+    console.log(mensagem);
+
+    if(nome='miriam'){     
+    }
+    if(email='miriamdesiqueira18@gmail.com'){
+    }
+
+    if(telefone='41996070136'){
+    }
+
+    if(mensagem='Quero saber mais sobre o blog'){
+    }
+})
+
+
+
